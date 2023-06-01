d6a63535d06d52a9864d6bdc4cf491cc42d3a4e1
Bryan Guerra <bryanmlg2018@gmail.com>
2023-06-01 11:40:10 -0600

    Open Authorization and Open ID connect

---
+++ sign-and-verifying/utils/token.js
@@ -0,0 +1,38 @@
+// Load environment variables
+import * as dotenv from "dotenv";
+dotenv.config();
+
+import jwt from "jsonwebtoken";
+import fs from "node:fs";
+const SECRET = process.env.SECRET;
+const ONE_MINUTE_IN_MILLISECONDS = 60 * 1000;
+const PRIVATE_KEY_PATH = process.env.PRIVATE_KEY_PATH;
+const PUBLIC_KEY_PATH = process.env.PUBLIC_KEY_PATH;
+export const signToken = (user) => {
+  const payload = {
+    sub: user.id,
+    name: user.fullname,
+    exp: Date.now() + ONE_MINUTE_IN_MILLISECONDS,
+  };
+
+  if(PRIVATE_KEY_PATH){
+    const privateKey = fs.readFileSync(PRIVATE_KEY_PATH, "utf8");
+    const token = jwt.sign(payload, privateKey, { algorithm: 'RS256'});
+    return token;
+  }  
+  return jwt.sign(payload, SECRET)
+};
+
+export const verifyToken = (token) => {
+  if(PUBLIC_KEY_PATH){
+    const publicKey = fs.readFileSync(PUBLIC_KEY_PATH, "utf8");
+    return jwt.verify(token, publicKey);
+  }
+  return jwt.verify(token, SECRET);
+};
+
+export const validateExpiration = (payload) => {
+  if (Date.now() > payload.exp) {
+    throw new Error("Token expired");
+  }
+};
